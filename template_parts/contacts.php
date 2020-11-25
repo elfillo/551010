25e904b06f57f62755e7da031525c99410069ec7
elf <filonenko0406@gmail.com>
2020-11-25 17:34:52 +0800

    индекс

--- template_parts/contacts.php
+++ template_parts/contacts.php
@@ -5,8 +5,9 @@
 			<div class="title"><span>Строй Сити</span><span class="title-description">Центральный офис</span></div>
 			<div class="info">
 				<div class="address">119002, Иркутск, ул. Трактовая, 4/3</div>
+				<div class="address">Индекс 664024</div>
 				<div class="phone"><a href="tel:+73952551010">+7 (3952) 55-10-10</a></div>
-				<div class="email">stroy_city@mail.ru</div>
+				<div class="email">stroi551010@yandex.ru</div>
 			</div>
 		</div>
 		<div class="contacts__content_map" id="newMap"></div>
